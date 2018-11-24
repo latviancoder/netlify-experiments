a321261135cc96506ecceb36bc26be2c9fd2f00d
rizovs <sergey.rizovs@3m5.de>
2018-11-24 18:53:50 +0100

    test firestore

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,24 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../call-for-spakers-a1d7e5b4fe4b.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'hello'
-    });
+  var docRef = db.collection('users').doc('alovelace');
+
+  var setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
+
+  callback(null, {
+    statusCode: 200,
+    body: 'hello'
+  });
 };
